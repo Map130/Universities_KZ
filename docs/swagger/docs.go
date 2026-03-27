7e1d8a2a66df761dd3c153a4bb768b13b1789679
map1e <map1e000@proton.me>
2026-03-27 15:17:01 +0500

    Again

--- docs/swagger/docs.go
+++ docs/swagger/docs.go
@@ -851,7 +851,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "165.22.26.188",
+	Host:             "10.114.0.2",
 	BasePath:         "/",
 	Schemes:          []string{"http", "https"},
 	Title:            "Universities KZ API",
